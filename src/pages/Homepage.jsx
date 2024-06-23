f53176972ab0f2ac57bf8bb1d3edfacdef484b08
Apirak Fakin <apirakfakin@gmail.com>
2024-06-23 20:17:21 +0700

    feat: add Navbar

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Homepage = () => {
+  return (
+    <section className="text-6xl text-blue-500 max-w-full bg-white">
+      <Navbar />
+      <main className="w-full">
+        hello world
+      </main>
+    </section>
+  );
+};
+
+export default Homepage;
